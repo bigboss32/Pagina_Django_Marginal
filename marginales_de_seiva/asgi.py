96ccea5a47663bceb62085d987e211dad455e3d1
bigboss32 <mig.garzon@udla.edu.co>
2023-07-14 01:13:46 -0500

    first commit

---
+++ marginales_de_seiva/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for marginales_de_seiva project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marginales_de_seiva.settings')
+
+application = get_asgi_application()
